ca53896218337bd406a375b0b79521766af9dadb
Jean Martin <nitram.naej2@gmail.com>
2016-10-17 19:39:12 +0200

    Change ouput format to fit the function MakeLeftRightHmm.

--- extractAllData.m
+++ extractAllData.m
@@ -1,4 +1,5 @@
-function [train_data, test_data, train_label, test_label] = extractAllData(words, folder, ratio)
+function [train_data_temp, test_data_temp, train_label, test_label, train_length, test_length] ...
+    = extractAllData(words, folder, ratio)
 
 %% Parameters of the dataset
 
@@ -34,7 +35,6 @@ end
 %% Shuffle the dataset
 %%
 
-
 featureShuffled = cell(nb_word, nb_sample);
 labelShuffled = zeros(nb_word, nb_sample);
 for i=1:nb_word
@@ -52,5 +52,33 @@ train_data = featureShuffled(:,1:threshold);
 train_label = labelShuffled(:,1:threshold);
 test_data = featureShuffled(:,threshold+1:end);
 test_label = labelShuffled(:,threshold+1:end);
+
+%% Convert to proper form
+%%
+
+train_length = zeros(nb_word,threshold);
+test_length = zeros(nb_word,nb_sample - threshold);
+
+train_data_temp = cell(nb_word,1);
+test_data_temp = cell(nb_word,1);
+
+for i=1:nb_word
+    word_temp = [];
+    for j=1:threshold
+        word_temp = cat(2,word_temp, featureShuffled{i,j});
+        train_length(i,j) = size(featureShuffled{i,j},2);
+    end
+    train_data_temp{i} = word_temp;
+    word_temp = [];
+    for j=threshold+1:nb_sample
+        word_temp = cat(2,word_temp, featureShuffled{i,j});
+        test_length(i,j - threshold) = size(featureShuffled{i,j},2);
+    end
+    test_data_temp{i} = word_temp;
+    
+end
+
+
+
 
 end
