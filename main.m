326770ed9544f9218de51a64c7009c32bf0d5388
Jean Martin <nitram.naej2@gmail.com>
2016-10-17 19:39:48 +0200

    Minor changes.

--- main.m
+++ main.m
@@ -1,4 +1,5 @@
 %% Parameters
+%%
 
 % clear;clc;
 
@@ -9,18 +10,33 @@ ratio = 0.8;
 nb_word = size(words, 2);
 
 
-
 %% Load data
+%%
 
-[train_data, test_data, train_label, test_label] = extractAllData(words, folder, ratio);
+
+[train_data, test_data, train_label, test_label, train_length, test_length] = extractAllData(words, folder, ratio);
+
 
 %% Creation HMM
-
+%%
 % Nb of states for hmm per words
-nb_class_word = [3 4 3 3 4 3 4 3 4 3 3];
+nb_class_word = [3 4 3 3 4 3 4 3 4 3 3]+2;
 
 
-hmm = 
 for i=1:nb_word
-    hmm = MakeLeftRightHMM(nStates,pD,obsData,lData);
+    hmm(i) = MakeLeftRightHMM(nb_class_word(i), GaussMixD, train_data{i}, train_length(i,:));
 end
+
+
+%almost working
+for j=1:3
+    offset = 0;
+    for i=1:nb_word
+        logP=logprob(hmm,test_data{i}(:,offset:offset + test_length(i,j)- 1));
+        [~,s(i,j)] = max(logP);
+        offset = offset + test_length(i,j);
+    end
+end
+
+
+
