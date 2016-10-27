fd682a6abd28d425fd532f8a506edf71db044880
Jean Martin <nitram.naej2@gmail.com>
2016-10-27 20:43:07 +0200

    Add argument to choose the output distribution.

--- trainAllHmm.m
+++ trainAllHmm.m
@@ -1,9 +1,9 @@
-function [HmmArray] = trainAllHmm(nb_state, train_data, train_length)
+function [HmmArray] = trainAllHmm(nb_state, train_data, train_length, Distrib)
 
 nb_word = size(train_data, 1);
 
 for j=1:nb_word
-    HmmArray(j) = MakeLeftRightHMM(nb_state(j), GaussMixD, train_data{j}, train_length(j,:));
+    HmmArray(j) = MakeLeftRightHMM(nb_state(j), Distrib, train_data{j}, train_length(j,:));
 end
 
 end
